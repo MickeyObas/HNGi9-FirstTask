4c72cdb3ea78a55e0ddcb05e0477381345b5e9c3
Michael <mikhzobby@gmail.com>
2022-10-27 15:18:13 +0100

    Sigh.

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.add_middleware(
 
 @app.get("/profile")
 def get_profile():
-    return {"slackUsername": "Michael Goke",
+    return {"slackUsername": "MichaelTheBrave",
             "backend": True,
             "age": 21,
             "bio": "I'm a third year Computer Science undegraduate here at FUTA. When i'm not geeking out over tech stuff, i enjoy watching anime, playing football, playing videogames and so much more! Looking forward to making more friends here. Ikuzoo!"}
