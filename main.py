e7d1753bb5a747922944c4b0436617a8ff070a4b
Michael <mikhzobby@gmail.com>
2022-10-27 14:46:16 +0100

    Added cors handling

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel # Might not need this for now.
 
 app = FastAPI()
 
-app.get("/")
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
 
 @app.get("/profile")
 def get_profile():
