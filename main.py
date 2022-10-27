0442656b81fe7dcc9dcba60abeadd5b1357d0a0a
Michael <mikhzobby@gmail.com>
2022-10-27 06:48:26 +0100

    First commit, of course :)

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/profile")
+def get_profile():
+    return {"slackUsername": "Michael Goke",
+            "backend": True,
+            "age": 21,
+            "bio": "I'm a third year Computer Science undegraduate here at FUTA. When i'm not geeking out over tech stuff, i enjoy watching anime, playing football, playing videogames and so much more! Looking forward to making more friends here. Ikuzoo!"}
+
